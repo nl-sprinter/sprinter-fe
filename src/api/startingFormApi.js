8e0bdb6a5461cd2aba050e8a5e56a35b39625575
kraftenty <kraftenty@gmail.com>
2025-04-09 20:33:40 +0900

    Fix: startingFormApi 수정 - formData받는부분 수정

--- src/api/startingFormApi.js
+++ src/api/startingFormApi.js
@@ -1,13 +1,19 @@
 import axiosInstance from './axiosInstance';
 
 export const sendStartingForm = async (formData) => {
+    console.log(JSON.stringify(formData));
+
     const response = await axiosInstance.post('/startingform/gptData', {
         projectName: formData.projectName,
         projectGoal: formData.projectGoal,
-        estimatedDuration: formData.projectDuration,
-        sprintCycle: formData.sprintCycle,
+        projectDomain: formData.projectDomain,
         teamMembers: parseInt(formData.teamMembers),
-        essentialFeatures: formData.keyRequirements
+        teamPositions: formData.teamPositions,
+        estimatedDuration: formData.estimatedDuration,
+        sprintCycle: formData.sprintCycle,
+        backlogDetailLevel: formData.backlogDetailLevel,
+        preferredTechStack: formData.preferredTechStack,
+        essentialFeatures: formData.essentialFeatures
     });
     return response.data;
 }; 
