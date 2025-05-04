22026ffc51508c4c5ae9b81076856950632e9b33
고경태 <kraftenty@gmail.com>
2025-05-04 22:02:23 +0900

    Fix: google login 주소 수정

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -67,7 +67,7 @@ const LoginPage = () => {
 
     const handleGoogleLogin = async () => {
         // alert("Google 로그인 기능은 도메인 문제로 현재 지원 중단 되었습니다.")
-        window.location.href = 'https://sprinter.kr/oauth2/authorization/google';
+        window.location.href = 'https://sprinter.kr/api/oauth2/authorization/google';
         // local: localhost:8080
     };
 
